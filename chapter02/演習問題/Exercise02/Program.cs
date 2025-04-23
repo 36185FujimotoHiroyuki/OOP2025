5a3309d81478dfc2057435ab604ade820dc1c481
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-04-23 11:04:17 +0900

    未完成

--- chapter02/演習問題/Exercise02/Program.cs
+++ chapter02/演習問題/Exercise02/Program.cs
@@ -23,12 +23,53 @@
         //  double meter = InchConverter.ToMeter(feet);
         // Console.WriteLine($"{feet}inch = {meter:0.0000}m");
         // }
-        
 
-       
+        // キーボードから入力(機能追加①)
 
+
+        // Console.WriteLine("変更アプリ");
+        // Console.WriteLine("１：インチからメートル");
+        //  Console.WriteLine("２：メートルからインチ");
+         //Console.Write("＞");
+
+        // int choice = int.Parse(Console.ReadLine());
+
+        // インチからメートルの変換
+        // if (choice == 1)
+        // {
+         //   Console.WriteLine("はじめ：");
+            int start = int.Parse(Console.ReadLine());
+          //Console.WriteLine("おわり：");
+             int end = int.Parse(Console.ReadLine());
+
+         static void PrintInchToMeterList(int start, int end) {
+            Console.WriteLine();
+            PrintInchToMeterList(start, end);
+       // }
+        //else if (choice == 2)
+       // {
+
+            // インチからメートルへの変換
+            for (int inch = start; inch <= end; inch++) {
+                double meter = inch * 0.0254;  
+                Console.WriteLine($"{inch} inch = {meter:0.0000} m");
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
 
 
 
