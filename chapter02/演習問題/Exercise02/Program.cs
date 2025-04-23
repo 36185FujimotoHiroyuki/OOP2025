e3d94ebc0bea67107d6c1a91c823dfb1780551af
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-04-23 10:25:42 +0900

    インチからメートルに変換

--- chapter02/演習問題/Exercise02/Program.cs
+++ chapter02/演習問題/Exercise02/Program.cs
@@ -11,10 +11,6 @@
             for (int inch = 1; inch <= 10; inch++) {
                 double meters = inch * inchToMeter;
                 Console.WriteLine($"{inch} インチ = {meters:F4} メートル");
-
-
-
-
             }
         }
 
@@ -23,9 +19,9 @@
         //       PrintfeetToMeterList(1, 10);
         //  }//インチからメートルへの対応表を出力
         //  static void PrintInchToMeterList(int start, int end) {
-        // for (int inch = start; inch <= end; inch++) {
-        //  double meter = InchConverter.ToMeter(inch);
-        // Console.WriteLine($"{inch}ih = {meter:0.0000}m");
+        // for (int feet = start; feet <= end; feet++) {
+        //  double meter = InchConverter.ToMeter(feet);
+        // Console.WriteLine($"{feet}inch = {meter:0.0000}m");
         // }
         
 
@@ -34,28 +30,6 @@
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-}
 
 
 
