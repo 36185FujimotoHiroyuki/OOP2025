7f2f1f178d8e7aea4173662cc79af7750a49db72
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-05-07 09:36:40 +0900

    FromApps追加

---
+++ FormApps/StopWatch/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace StopWatch {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.lbTimeDisp = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.btReset = new System.Windows.Forms.Button();
+            this.tmDispTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbTimeDisp
+            // 
+            this.lbTimeDisp.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbTimeDisp.Font = new System.Drawing.Font("ＭＳ Ｐ明朝", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbTimeDisp.Location = new System.Drawing.Point(12, 22);
+            this.lbTimeDisp.Name = "lbTimeDisp";
+            this.lbTimeDisp.Size = new System.Drawing.Size(285, 54);
+            this.lbTimeDisp.TabIndex = 0;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(20, 101);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(121, 54);
+            this.btStart.TabIndex = 1;
+            this.btStart.Text = "スタート";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(85, 172);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(121, 54);
+            this.btStop.TabIndex = 2;
+            this.btStop.Text = "ストップ";
+            this.btStop.UseVisualStyleBackColor = true;
+            // 
+            // btReset
+            // 
+            this.btReset.Location = new System.Drawing.Point(159, 101);
+            this.btReset.Name = "btReset";
+            this.btReset.Size = new System.Drawing.Size(121, 54);
+            this.btReset.TabIndex = 3;
+            this.btReset.Text = "リセット";
+            this.btReset.UseVisualStyleBackColor = true;
+            // 
+            // tmDispTimer
+            // 
+            this.tmDispTimer.Tick += new System.EventHandler(this.tmDispTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(314, 280);
+            this.Controls.Add(this.btReset);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.lbTimeDisp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTimeDisp;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Button btReset;
+        private System.Windows.Forms.Timer tmDispTimer;
+    }
+}
+
---
+++ FormApps/UnitConverter/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace UnitConverter {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            label1 = new Label();
+            tbBeforeConversion = new TextBox();
+            tbAfterConversion = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label1.Location = new Point(82, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(218, 45);
+            label1.TabIndex = 0;
+            label1.Text = "距離換算アプリ";
+            // 
+            // tbBeforeConversion
+            // 
+            tbBeforeConversion.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            tbBeforeConversion.Location = new Point(103, 130);
+            tbBeforeConversion.Name = "tbBeforeConversion";
+            tbBeforeConversion.Size = new Size(173, 39);
+            tbBeforeConversion.TabIndex = 1;
+            tbBeforeConversion.TextChanged += tbBeforeConversion_TextChanged;
+            // 
+            // tbAfterConversion
+            // 
+            tbAfterConversion.Font = new Font("Yu Gothic UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            tbAfterConversion.Location = new Point(103, 316);
+            tbAfterConversion.Name = "tbAfterConversion";
+            tbAfterConversion.Size = new Size(173, 39);
+            tbAfterConversion.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(103, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 46);
+            button1.TabIndex = 2;
+            button1.Text = "変換";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += ConvertButton_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(511, 346);
+            button2.Name = "button2";
+            button2.Size = new Size(8, 8);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(389, 444);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(tbAfterConversion);
+            Controls.Add(tbBeforeConversion);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox tbBeforeConversion;
+        private TextBox tbAfterConversion;
+        private Button button1;
+        private Button button2;
+    }
+}
