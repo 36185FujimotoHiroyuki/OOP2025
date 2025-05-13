c144281e50acbb9b31d8488604a1b138b86a8b8d
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-05-13 10:29:25 +0900

    Chapter2演習問題2.1.1 と2.1.2　完成

---
+++ chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {//2.1.1
+    public class Song {
+        //クラス定義
+        String Title;
+        String ArtistName;
+        int Length;
+       
+        
+        //2.1.2 コンストラクター定義
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+
+        //コンストラクター
+        //  public string Title { get ; set; }
+        //  public string ArtistName { get; set; }
+        // public int Length { get; set; }
+
+        
+       // public string Title { get; set; } = string.Empty;
+       
+      //  public string ArtistName { get; set; } = string.Empty;
+    
+       // public int Length { get; set; }
+
+
+
+    }
+
+    }
