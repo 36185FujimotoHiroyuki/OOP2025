95db6fc3a6590ad4c16a0b98212f32eb1b33baa3
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-05-16 14:02:35 +0900

    p65まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Section01 {
     internal class Program {
 
-        public delegate bool judgement(int value);//デリゲートの宣言
+       
 
 
         static void Main(string[] args) {
@@ -11,20 +11,13 @@
 
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            judgement judge = IsEven;
-            Console.WriteLine(Count(numbers,judge));
+           
+            Console.WriteLine(Count(numbers,delegate(int n) { return n % 2 == 0; }));
 
         }
-        //メソッドへ渡す処理
-        static bool IsEven(int n) {
-            return n % 2 == 1;
-        }
 
-
-
-
-
-            static int Count(int[] numbers, judgement judge) {
+       
+            static int Count(int[] numbers, Predicate<int> judge) {
              
                 var count = 0;
                 foreach (var n in numbers) {
