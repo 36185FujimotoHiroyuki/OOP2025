cdd9411af46af166af13c0a2954516076ca99e90
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-05-23 14:46:41 +0900

    練習問題P112Exercise2まで完成　

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -15,11 +17,36 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-          
-        }
+            //Sを含むものを取り出す文
+            //foreach文    
+            foreach (var lang in langs) {
 
+                if (lang.Contains("S"))
+
+                    Console.WriteLine(lang);
+            }
+            //for文    
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('s'))
+                    Console.WriteLine(langs[i]);
+
+            }
+            Console.WriteLine("");//改行
+
+            //while文 
+            int index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+            }
+        }
         private static void Exercise2(List<string> langs) {
-            
+            //リンク
+            var selected = langs.Where(x => x.Contains('S'));
+            foreach(var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
