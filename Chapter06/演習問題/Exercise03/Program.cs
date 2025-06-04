dc4a756a7308e4105c04967331b773bba334e5c2
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-06-04 11:28:16 +0900

    p153演習問題6.3

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Globalization;
+using System.Text;
 
 namespace Exercise03 {
     internal class Program {
@@ -20,11 +21,64 @@ namespace Exercise03 {
 
             Console.WriteLine("6.3.5");
             Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
         }
 
+        private static void Exercise6(string text) {
+            var letter = text.ToLower().Replace(" ", " ");
+            // var letter = text.ToLower();
+            //  .Where(char.IsLetter)
+            // .GroupBy(c => c)
+            // .Select(group => new { Letter = group.Key, Count = group.Count() })
+            // .OrderBy(result => result.Letter)
+            // .ToList();
+
+            // Console.WriteLine("出現アルファベットの個数:");
+            // foreach (var item in letter) {
+            {
+
+                //   Console.WriteLine($"{item.Letter}:{item.Count}");
+//辞書で集計
+                var alphDicCount = Enumerable.Range('a', 26)
+                    .ToDictionary(num => ((char)num).ToString(), num => 0);
+
+                foreach (var alph in letter) {
+                    alphDicCount[alph.ToString()]++;
+                }
+                foreach (var item in alphDicCount) {
+                    Console.WriteLine($"{item.Key}:{item.Value}");
+
+
+                    //配列の集計
+
+                    var array = Enumerable.Repeat(0, 26).ToArray();
+                    foreach(var alph in letter) {
+                        array[alph - 'a']++;
+                    }
+                    for (char ch = 'a'; ch <= 'a'; ch++) {
+                        Console.WriteLine($"{ch}:{array[ch - 'a']}");
+                    }
+                    Console.WriteLine();
+
+            //'a'から順にカウントして出力
+
+                    for (char ch ='a'; ch <= 'z'; ch++) {
+                        Console.WriteLine($"{ch}:{text.Count(tc => tc == ch)}");
+                }
+
+
+
+
+
+
+                }
+            }
+        }
         private static void Exercise1(string text) {
             var spaces = text.Count(c => c == ' ');
-            //(char.IsWhiteSpace);別回答
+                                 //(char.IsWhiteSpace);別回答
             Console.WriteLine("空白数：", spaces);
         }
         #region
@@ -49,8 +103,16 @@ namespace Exercise03 {
 
         private static void Exercise3(string text) {
 
-        }
+            var array = text.Split(' ');
+            var sb = new StringBuilder(array[0]);
+            foreach (var word in array.Skip(1)) {
 
+               sb.Append(word + " "); 
+          }    
+              string result = sb.ToString().TrimEnd();
+               Console.WriteLine(result);
+
+            }
         private static void Exercise4(string text) {
             //正解コード
             var count = text.Split(' ').Length;
---
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var line = "Novelist=谷崎潤一郎；BestWork=春琴抄;Born =1886";
+
+            var array = line.Split(':');
+
+            foreach (var pair in line.Split(';')) { 
+            var word = pair.Split('=');
+            Console.WriteLine($"{ToJapanese(word[0])}:{word[1]}");
+        }
+       }
+
+
+          
+         //   var jpName = ToJapanese(word[0]);
+
+            // foreach (var kvp in result) {
+            //  Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            //  }
+        
+        //<sumary>
+        //引数の単語を日本語へ変換
+
+        //</summary>
+        //<param name="Key">"Novelist","BestWork","Born"</param>
+        //<returns>"「作家」,「代表作」,「誕生年」,</returns>
+
+        static string ToJapanese(string key) {
+
+            return key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+           _=> "引数keyは、正しい値ではありません"
+        };
+
+            //switch (key){
+            //case"Novelist";
+            // return "作家";
+            //case "BestWork"
+            //return"代表作";
+            //case"Born";
+            //return"誕生年";
+            //default;
+            //return"引数keyは、正しい値ではありません";
+            //}
+
+
+
+
+
+        }
+    }
+
+}
+
+
+
+
