4ed53e1fc642de3969442352d948f057ea4cafdc
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-06-17 12:06:20 +0900

    設問１＿提出（途中）

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -16,8 +16,25 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
+            //   public IEnumerable<Sale> ReadSales(string filePath) {
+
+            var student = new List<Student>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (var line in lines) {
+                String[] items = line.Split(',');
+
+                Student sale = new Student() {
+
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Student(items[2]),
 
 
+                };
+                Student.Add(student);
+            }
         }
 
         //メソッドの概要： 
