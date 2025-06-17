c4aa2ce077d2878ce697e48fe0883c2ffe67e475
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-06-17 11:53:35 +0900

    設問１_提出（途中）

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -1,29 +1,38 @@
-﻿namespace Test01 {
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Test01 {
     public class ScoreCounter {
         private IEnumerable<Student> _score;
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
 
+            _score = ReadScore(filePath);
 
         }
 
+
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
 
 
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-            
 
 
-
+            var score = new SortedDictionary<string, int>();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
 
         }
     }
-}
+
+
+
+
+    }
+    
+
--- Test01/Test01/Student.cs
+++ Test01/Test01/Student.cs
@@ -9,10 +9,10 @@ namespace Test01 {
     public class Student {       //プロパティ
 
         //学生の名前
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
-        //科目名
-        public string Subject { get; set; }
+        //科目名= 
+        public string Subject { get; set; } = string.Empty;
         //点数
         public int Score { get; set; }
 
