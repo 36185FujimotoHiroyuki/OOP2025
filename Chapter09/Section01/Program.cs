bc900bac48dbef5bb3a6840108871c28d4aa25d1
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-06-27 10:14:14 +0900

    Exercise01完成

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -2,6 +2,8 @@
 
 namespace Section01 {
     internal class Program {
+       // private static object culture;
+
         static void Main(string[] args) {
 
 
@@ -35,19 +37,32 @@ namespace Section01 {
 
 
             //①[自分の生年月日は何曜日かをプログラムを書いて調べる]
-
+//正解
             Console.Write("西暦：");
             var year = int.Parse(Console.ReadLine());
 
-            Console.Write("西暦：");
+            Console.Write("月：");
             var month = int.Parse(Console.ReadLine());
 
-            Console.Write("西暦：");
+            Console.Write("日：");
             var day = int.Parse(Console.ReadLine());
 
             var birth = new DateTime(year, month, day);
 
-            var colture = new CultureInfo("ja-JP");
+
+            var culture = new CultureInfo("ja-JP");
+
+
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            var str = birth.ToString("ggyy年M月d日",culture);
+
+            var shortDayWeek = culture.DateTimeFormat.GetShortestDayName(birth.DayOfWeek);
+
+            //Console.WriteLine(str + shortDayOfweek + "曜日");
+            Console.WriteLine(str + birth.ToString("ddd曜日", culture));
+
+
 
 
             //  var today =  new DateTime(2005, 12, 20);
@@ -61,6 +76,13 @@ namespace Section01 {
 
             //   }
             //」
+
+
+
+
+
+
+
 
 
             //②うるう年
@@ -90,10 +112,28 @@ namespace Section01 {
 
             Console.WriteLine($"生まれてから {ikitahi} 日目です。");
 
+            //③の別回答
+            // TimeSpan diff = DateTime.Now - birth;
+            // Console.WriteLine(diff.TotalDays + "日");
+
+
+            //③の別回答
+           // TimeSpan diff;
+          //  while (true) {
+           //     diff = DateTime.Now - birth;
+           //     Console.WriteLine($"\r{diff.TotalSeconds}秒");//生まれてからの経過秒数
+           // }
+
+            //④あなたは〇〇歳です
+
+          // int age = GetAge(birth,DateTime,today)
+
+
+
 
 
         }
-        }
+    }
     }
 
 
---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+           
+            var dt = new DateTime(2024, 3, 9, 19, 3, 9);
+
+            Console.WriteLine(dt.ToString("yyyy/MM/dd HH:mm"));
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            //DateTime.ToStringを使った例
+
+            var dt = new DateTime(2024, 3, 9, 19, 3, 9);
+             Console.WriteLine(dt.ToString("yyyy年MM月dd日　HH時mm分ss秒"));
+          
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var dt = new DateTime(2024, 3, 9, 19, 3, 9);
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+               Console.WriteLine(dt.ToString("ggy年M月d日(dddd)", culture));
+        }
+    }
+    }
+
+
