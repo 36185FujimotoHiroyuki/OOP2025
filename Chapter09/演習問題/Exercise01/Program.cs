fcc78750de8333aca803132ab11e964fc8d31aec
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-06-27 13:48:11 +0900

    Exercise03完成

--- Chapter09/演習問題/Exercise01/Program.cs
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -28,7 +28,39 @@ namespace Exercise01 {
             var dt = new DateTime(2024, 3, 9, 19, 3, 9);
             var culture = new CultureInfo("ja-JP");
             culture.DateTimeFormat.Calendar = new JapaneseCalendar();
-               Console.WriteLine(dt.ToString("ggy年M月d日(dddd)", culture));
+            Console.WriteLine(dt.ToString("ggy年M月d日(dddd)", culture));
+
+            //正解コード
+
+            //   private static void DisplayDatePattern1(DateTime dateTime) {
+
+            //  var str = String.Format($"{dateTime:yyyy/MM/dd HH:mm}");
+            //  Console.WriteLine(str);
+            //  }
+
+            //    private static void DisplayDatePattern2(DateTime dateTime) {
+            //  var str = String.Format($"{dateTime:yyyy年MM月dd日 HH時:mm分ss秒}");
+            //  Console.WriteLine(str);
+            //  }
+
+            //    private static void DisplayDatePattern3(DateTime dateTime) {
+            // 
+            //      var culture = new CultureInfo("ja-JP");
+            //      culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+           // 和暦２桁表示（ゼロサプレスなし）
+            var datestr = dateTime.ToString("ggy", culture);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek);
+
+            var str = string.Format($"{datestr}年{dateTime.Month,2}月{dateTime.Day,2}日({dayOfWeek})");
+            Console.WriteLine(str);
+
+            //和暦２桁表示（ゼロサプレスあり）
+            var cul = dateTime.ToString("gg", culture);
+            var year = int.Parse(dateTime.ToString("yy", culture));
+            var str2 = string.Format($"{cul}{year,2}年{dateTime.Month,2}月{dateTime.Day,2}日({dayOfWeek})");
+            Console.WriteLine(str2);
+
+
         }
     }
     }
---
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+           
+                var tw = new TimeWatch();
+                tw.Start();
+                // スリープする
+                Thread.Sleep(1000);
+                TimeSpan duration = tw.Stop();
+                Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
+
+            }
+        }
+
+        class TimeWatch {
+            private DateTime _time;
+
+            public void Start() {
+            //現在の時間を_timeに設定
+
+            _time = DateTime.Now;
+            
+
+        }
+
+        public TimeSpan Stop() {
+            //経過時間を返却する
+            
+
+            var stop = DateTime.Now - _time;
+
+            return stop;
+
+
+
+            //return new TimeSpan(); // ←エラーを出さないためだけのダミー（使い方も参考にしない）
+        
+        }
+    }
+}
