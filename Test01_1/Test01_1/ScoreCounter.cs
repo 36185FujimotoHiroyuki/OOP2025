d9b84ad7d6e7c4e1607eb00b3575e17624c263c9
博幸 藤本 <oibc36185@ojs.ac.jp>
2025-07-08 12:52:14 +0900

    練習用

---
+++ Test01_1/Test01_1/ScoreCounter.cs
@@ -0,0 +1,47 @@
+﻿namespace Test01_1 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+
+            _score = ReadScore("./StudentScore.csv");
+        }
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+
+            List<Student> score = new List<Student>();
+            //ファイルを一気に読み込み
+            string[] lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (string line in lines) {
+                String[] items = line.Split(',');
+                //Saleオブジェクトを生成
+                Student student = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                score.Add(student);
+            }
+            return score;
+
+
+
+        }
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerstoreScore() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject)) {
+                    dict[score.Subject] += score.Score;
+                } else {
+                    dict[score.Subject] = score.Score;
+                }
+            }
+            return dict;
+        }
+    }
+}
