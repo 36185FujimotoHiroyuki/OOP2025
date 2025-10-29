13d1b5ff97daa62e41d20acdcb47c45371a5e52e
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-10-29 10:23:35 +0900

    演習問題４完成

--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -21,41 +21,73 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2() {
-  // var expensiveBooks = Library.Books.Where(b => b.Price == maxPrice);
-            
-            var book = Library.Books.Max(b => b.Price);
+            // var expensiveBooks = Library.Books.Where(b => b.Price == maxPrice);
+
+            var book = Library.Books.MaxBy(book => book.Price);
             Console.WriteLine(book);
-         
+
         }
 
         private static void Exercise1_3() {
             var countsByYear = Library.Books
-       .GroupBy(b => b.PublishedYear)      
-       .Select(g => new { Year = g.Key, Count = g.Count() }) 
-       .OrderBy(g => g.Year);               
+                 //  .GroupBy(book => book.PublishedYear)      
+                 // .Select(group => new { Year = group.Key, Count = group.Count() }) 
+                 // .OrderBy(group => group.Year);               
 
+                 // foreach (var item in countsByYear) {
+                 //    Console.WriteLine($"{item.Year}年: {item.Count}");
+
+
+                 .GroupBy(b => b.PublishedYear)
+                  .OrderBy(b => b.Key)
+                  .Select(b => new {
+                      PublishedYear = b.Key,
+                      Count = b.Count()
+                  });
             foreach (var item in countsByYear) {
-                Console.WriteLine($"{item.Year}年: {item.Count}");
+                Console.WriteLine($"{item.PublishedYear}年: {item.Count}");
+
+
             }
         }
         private static void Exercise1_4() {
-          
-        }
+            var sortedBooks = Library.Books
+           
+            .OrderByDescending(b => b.PublishedYear)
+            .ThenByDescending(b => b.Price);
 
+            foreach (var book in sortedBooks) {
+                Console.WriteLine($"{book.PublishedYear}年 {book.Price}円 {book.Title}");
+
+            }
+
+        }
         private static void Exercise1_5() {
+
+            var Books = Library.Books
+       .Where(book => book.PublishedYear == 2022)
+       .Select(book => book.CategoryId)
+       .Distinct()
+       .OrderBy(category => category);
       
+
+            foreach (var category in Books) {
+                Console.WriteLine(category);
+            }
+
+
         }
 
         private static void Exercise1_6() {
-   
+
         }
 
         private static void Exercise1_7() {
-          
+
         }
 
         private static void Exercise1_8() {
-        
+
         }
     }
 }
