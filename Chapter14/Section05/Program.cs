fcbe171e7418e12bcf37f404a72ce5ee67db5be7
FujimotoHiroyuki <oibc36185@ojs.ac.jp>
2025-11-06 15:17:07 +0900

    並列処理ありなし比較Sample

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("並列処理あり");
+
+            Parallel.For(0, 5, i => {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            });
+            for (int i = 0; i < 5; i++) {
+                Console.WriteLine($"処理{i}開始");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理{i}終了");
+            }
+
+
+    }
+}
+}
